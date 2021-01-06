e5be53ec08e997ccb647908caf1f2ccad35412a6
petroskyrkos <57561774+petroskyrkos@users.noreply.github.com>
2021-01-06 17:21:37 +0200

    a
    a

--- Personal_Site/includes/navBar.php
+++ Personal_Site/includes/navBar.php
@@ -21,7 +21,7 @@
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
         <li class="nav-item ">
-          <a class="nav-link" href="main.php">Home </a>
+          <a class="nav-link" href="index.php">Home </a>
         </li>
         <!-- <li class="nav-item">
           <a class="nav-link " href="news.php">News</a>
